c0474c3a0972b5ba8fd382dcfde84797ebbda261
Anyushu <suzuki@lamp.jp>
2021-01-09 16:26:54 +0900

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,86 @@
+//path モジュールの読み込み
 const path = require("path");
-const webpack = require("webpack");
+//MiniCssExtractPlugin の読み込み
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
-const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
-module.exports = (env, argv) => ({
+module.exports = {
   entry: "./assets/js/index.js",
   output: {
     path: path.resolve(__dirname, "dist/js"),
     filename: "bundle.js",
   },
-  optimization: {
-    minimizer: [new TerserPlugin({}), new OptimizeCssAssetsPlugin({})],
-  },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        // 対象となるファイルの拡張子(scss)
+        test: /\.scss$/,
+        // Sassファイルの読み込みとコンパイル
         use: [
+          // CSSファイルを抽出するように MiniCssExtractPlugin のローダーを指定
           {
-            loader: "babel-loader",
-            options: {
-              presets: ["@babel/preset-env"],
-            },
+            loader: MiniCssExtractPlugin.loader,
           },
-        ],
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
+          // CSSをバンドルするためのローダー
           {
             loader: "css-loader",
             options: {
-              url: false,
+              // ソースマップを有効に
+              sourceMap: true,
+              // css-loader の前に適用されるローダーの数を指定
+              importLoaders: 2,
+            },
+          },
+          // PostCSS
+          {
+            loader: "postcss-loader",
+            options: {
+              // PostCSS でもソースマップを有効に
+              sourceMap: true,
+              postcssOptions: {
+                // ベンダープレフィックスを自動付与
+                plugins: ["autoprefixer"],
+              },
+            },
+          },
+          // Sass をコンパイルするローダー
+          {
+            loader: "sass-loader",
+            options: {
+              // dart-sass を優先
+              implementation: require("sass"),
+              sassOptions: {
+                // fibers を使わない場合は以下で false を指定
+                fiber: require("fibers"),
+              },
               sourceMap: true,
             },
           },
-          "sass-loader",
+        ],
+      },
+      {
+        //file-loader の対象となるファイルの拡張子
+        test: /\.(gif|png|jpe?g|svg|eot|wof|woff|ttf)$/i,
+        use: [
+          {
+            //画像を出力フォルダーにコピーするローダー
+            loader: "file-loader",
+            options: {
+              // 画像ファイルの名前とパスの設定
+              name: "../images/[name].[ext]",
+            },
+          },
         ],
       },
     ],
   },
+  //プラグインの設定
   plugins: [
-    new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery",
-      Popper: ["popper.js", "default"],
-    }),
+    //MiniCssExtractPlugin プラグインのインスタンスを生成
     new MiniCssExtractPlugin({
+      //出力される CSS のファイル名を指定
       filename: "../css/style.css",
     }),
   ],
-  performance: {
-    hints: false,
-  },
-});
+  //source-map タイプのソースマップを出力
+  devtool: "source-map",
+};
